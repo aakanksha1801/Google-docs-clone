12c438f2726ae4cdc3de66ed84f113666bf3fe89
Aakanksha Jaiswar <aakankshajaiswar_it20b9_58@dtu.ac.in>
2022-08-27 16:50:27 +0530

    initial commit

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose  from 'mongoose';
+
+const Connection = async (username = '', password = 'c') => {
+    const URL = `mongodb://${username}:${password}`;
+
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {   
+        console.log('Error while connecting with the database ', error);
+    }
+}
+
+export default Connection;
